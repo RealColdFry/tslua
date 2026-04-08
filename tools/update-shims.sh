dfc0943cb932d447136d76a72ba1d2510d845a1b
Cold Fry <coldfry.business@gmail.com>
2026-04-08 02:05:30 +0000

    update-shims: reset to submodule pointer, idempotent

--- tools/update-shims.sh
+++ tools/update-shims.sh
@@ -4,13 +4,21 @@
 # Usage: ./tools/update-shims.sh
 #
 # This script:
-# 1. Applies patches to extern/typescript-go
-# 2. Updates shim go.mod files to point at the current tsgo commit
-# 3. Regenerates shim.go files via gen_shims
+# 1. Resets extern/typescript-go to the committed submodule pointer
+# 2. Applies patches on top
+# 3. Updates shim go.mod files to point at the (unpatched) tsgo commit
+# 4. Regenerates shim.go files via gen_shims
+# 5. Verifies the build
 
+cd "$(git rev-parse --show-toplevel)"
+
+# Reset tsgo to the clean submodule pointer, then apply patches
 pushd extern/typescript-go
+git checkout "$(git -C ../.. rev-parse HEAD:extern/typescript-go)"
 TSGO_COMMIT="$(git rev-parse HEAD)"
-git am --3way --no-gpg-sign ../../patches/tsgo/*.patch
+if ls ../../patches/tsgo/*.patch &>/dev/null; then
+    git am --3way --no-gpg-sign ../../patches/tsgo/*.patch
+fi
 popd
 
 find ./shim -type f -name 'go.mod' -execdir go get -x "github.com/microsoft/typescript-go@$TSGO_COMMIT" \; -execdir go mod tidy -v \;
